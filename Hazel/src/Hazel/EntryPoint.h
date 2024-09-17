0eaecfe39376ebe31aabe3cbc6f9e2e7b6b95afd
hoho965191737 <965191737@qq.com>
2024-09-17 10:28:12 +0800

    setup log system

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -5,6 +5,11 @@
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
+	Hazel::Log::Init();
+
+	HZ_CORE_WARN("Initialized Log!");
+	HZ_INFO("Hello! argc={0}", argc);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
