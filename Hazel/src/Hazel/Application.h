347137451fd90098ac30981f79b5a36ff6173833
hoho965191737 <965191737@qq.com>
2024-10-09 22:18:03 +0800

    window events

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Hazel/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Hazel {
@@ -14,10 +16,13 @@ namespace Hazel {
 
 		void Run();
 
+		void OnEvent(Event& e);
+
 	private:
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 
+		bool OnWindowClose(WindowCloseEvent& e);
 	};
 
 	// to be defined in client
