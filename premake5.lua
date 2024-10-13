47269d436b01b90e2ca3df0fedc25ba739622f7a
hoho965191737 <965191737@qq.com>
2024-10-13 13:42:53 +0800

    add glad sys

--- premake5.lua
+++ premake5.lua
@@ -12,8 +12,10 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 -- Include directories relative to root folder (solution directory)
 IncludeDir = {}
 IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
+IncludeDir["Glad"] = "Hazel/vendor/Glad/include"
 
 include "Hazel/vendor/GLFW"
+include "Hazel/vendor/Glad"
 
 project "Hazel"
 	location "Hazel"
@@ -36,10 +38,12 @@ project "Hazel"
 		"%{wks.location}/Hazel/src",
 		"%{wks.location}/Hazel/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
 	}
 
 	links{
 		"GLFW",
+		"Glad",
 		"opengl32.lib",
 	}
 
@@ -51,6 +55,7 @@ project "Hazel"
 		defines{
 			"HZ_PLATFORM_WINDOWS",
 			"HZ_BUILD_DLL",
+			"GLFW_INCLUDE_NONE",
 		}
 		
 	filter "configurations:Debug"
