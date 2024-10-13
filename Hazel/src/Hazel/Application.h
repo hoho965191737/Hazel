3d08cfa06e5a8be9fd3a73cc9373e88a114b0a26
hoho965191737 <965191737@qq.com>
2024-10-13 14:57:23 +0800

    add imgui sys

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -23,12 +23,17 @@ namespace Hazel {
 		void PushLayer(Layer* layer);
 		void PushOverlay(Layer* layer);
 
+		inline Window& GetWindow() { return *m_Window; }
+		inline static Application& Get() { return *s_Instance; }
+
 	private:
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 
 		bool OnWindowClose(WindowCloseEvent& e);
 		LayerStack m_LayerStack;
+		static Application* s_Instance;
+
 	};
 
 	// to be defined in client
--- premake5.lua
+++ premake5.lua
@@ -13,9 +13,11 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 IncludeDir = {}
 IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
 IncludeDir["Glad"] = "Hazel/vendor/Glad/include"
+IncludeDir["ImGui"] = "Hazel/vendor/imgui"
 
 include "Hazel/vendor/GLFW"
 include "Hazel/vendor/Glad"
+include "Hazel/vendor/imgui"
 
 project "Hazel"
 	location "Hazel"
@@ -39,11 +41,13 @@ project "Hazel"
 		"%{wks.location}/Hazel/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
+		"%{IncludeDir.ImGui}",
 	}
 
 	links{
 		"GLFW",
 		"Glad",
+		"ImGui",
 		"opengl32.lib",
 	}
 
