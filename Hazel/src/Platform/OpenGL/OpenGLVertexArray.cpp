7097cef3d32ed2bf112ccdb5cf54e37e16c6b16a
hoho965191737 <965191737@qq.com>
2024-11-16 16:10:06 +0800

    modify refs scopes and smart pointers

--- Hazel/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -50,7 +50,7 @@ namespace Hazel {
 	}
 
 
-	void OpenGLVertexArray::AddVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer)
+	void OpenGLVertexArray::AddVertexBuffer(const Ref<VertexBuffer>& vertexBuffer)
 	{
 		HZ_CORE_ASSERT(vertexBuffer->GetLayout().GetElements().size(), "Vertex Buffer has no layout!");
 		
@@ -73,7 +73,7 @@ namespace Hazel {
 	}
 
 
-	void OpenGLVertexArray::SetIndexBuffer(const std::shared_ptr<IndexBuffer>& indexBuffer)
+	void OpenGLVertexArray::SetIndexBuffer(const Ref<IndexBuffer>& indexBuffer)
 	{
 		glBindVertexArray(m_RendererID);
 		indexBuffer->Bind();
