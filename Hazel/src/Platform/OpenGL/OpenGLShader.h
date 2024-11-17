45bb61e6f5f7a5673435300f36eaeb6ec381b84f
hoho965191737 <965191737@qq.com>
2024-11-17 12:58:04 +0800

    add shader asset files

--- Hazel/src/Platform/OpenGL/OpenGLShader.h
+++ Hazel/src/Platform/OpenGL/OpenGLShader.h
@@ -1,12 +1,17 @@
 #pragma once
 #include "Hazel/Renderer/Shader.h"
+
 #include <glm/glm.hpp>
+
+// TODO: REMOVE!
+typedef unsigned int GLenum;
 
 namespace Hazel {
 	class OpenGLShader: public Shader
 	{
 	public:
 		OpenGLShader(const std::string& vertexSrc, const std::string& fragmentSrc);
+		OpenGLShader(const std::string& filepath);
 		virtual ~OpenGLShader();
 		virtual void Bind() const override;
 		virtual void Unbind() const override;
@@ -20,6 +25,11 @@ namespace Hazel {
 
 		void UploadUniformMat3(const std::string& name, const glm::mat3& matrix);
 		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
+	
+	private:
+		std::string ReadFile(const std::string& filepath);
+		std::unordered_map<GLenum, std::string> PreProcess(const std::string& source);
+		void Compile(const std::unordered_map<GLenum, std::string>& shaderSources);
 
 	private:
 		uint32_t m_RendererID;	// 唯一标识这个对象类型的数字在opengl中
