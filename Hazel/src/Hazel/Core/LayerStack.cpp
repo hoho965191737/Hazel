d4d097187acaac1a8d97b5ffdd444c237abe0b9e
hoho965191737 <965191737@qq.com>
2024-11-23 15:52:51 +0800

    call layer's ondetach before delet and todo

--- Hazel/src/Hazel/Core/LayerStack.cpp
+++ Hazel/src/Hazel/Core/LayerStack.cpp
@@ -7,8 +7,10 @@ namespace Hazel {
 	}
 	LayerStack::~LayerStack()
 	{
-		for (Layer* layer : m_Layers)
+		for (Layer* layer : m_Layers) {
+			layer->OnDetach();
 			delete layer;
+		}
 	}
 	void LayerStack::PushLayer(Layer* layer)
 	{
