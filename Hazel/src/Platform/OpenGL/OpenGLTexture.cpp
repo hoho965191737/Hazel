65c014aad605c9b0f9bd1969aa998a1bb2fbf2b8
hoho965191737 <965191737@qq.com>
2024-11-30 12:20:09 +0800

    Use template instead of raw pointers (mem leak) and a single shader to render both textured and non-textured

--- Hazel/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -4,6 +4,18 @@
 #include <glad/glad.h>
 
 namespace Hazel {
+	OpenGLTexture2D::OpenGLTexture2D(uint32_t width, uint32_t height)
+		: m_Width(width), m_Height(height)
+	{
+		m_InternalFormat = GL_RGBA8;
+		m_DataFormat = GL_RGBA;
+		glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
+		glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Width, m_Height);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	}
 	
 	OpenGLTexture2D::OpenGLTexture2D(const std::string& path)
 		: m_Path(path)
@@ -11,6 +23,7 @@ namespace Hazel {
 		int width, height, channels;
 		stbi_set_flip_vertically_on_load(1);
 		stbi_uc* data = stbi_load(path.c_str(), &width, &height, &channels, 0);
+		
 		HZ_CORE_ASSERT(data, "Failed to load image!");
 		m_Width = width;
 		m_Height = height;
@@ -26,6 +39,9 @@ namespace Hazel {
 			internalFormat = GL_RGB8;
 			dataFormat = GL_RGB;
 		}
+
+		m_InternalFormat = internalFormat;
+		m_DataFormat = dataFormat;
 		HZ_CORE_ASSERT(internalFormat & dataFormat, "Format not supported!");
 
 		glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
@@ -50,5 +66,12 @@ namespace Hazel {
 	void OpenGLTexture2D::Bind(uint32_t slot) const
 	{
 		glBindTextureUnit(slot, m_RendererID);
+	}
+
+	void OpenGLTexture2D::SetData(void* data, uint32_t size)
+	{
+		uint32_t bpp = m_DataFormat == GL_RGBA ? 4 : 3;
+		HZ_CORE_ASSERT(size == m_Width * m_Height * bpp, "Data must be entire texture!");
+		glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);
 	}
 }
